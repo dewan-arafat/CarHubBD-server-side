dd1fa8cacfc0efb969299a00f8d99c86f99c9ec1
dewan-arafat <dewanarafat1@gmail.com>
2022-11-25 23:54:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.awyxyaj.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('ReasleBD server is running')
+})
+
+app.listen(port, () => {
+    console.log(`ResaleBD server running on ${port}`);
+})
